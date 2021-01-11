abe7145e81b87fbe8306e7cfd0db96779aa9d1a0
dmeiri <dmeiri@cisco.com>
2021-01-11 19:17:59 +0200

    fixes

--- pure/wsgi_app.py
+++ pure/wsgi_app.py
@@ -2,10 +2,6 @@ from time import sleep
 from os import getpid
 
 
-DELAY = 2
-
-
 def application(env, start_response):
-    sleep(DELAY)
     start_response('200 OK', [('Content-Type', 'text/html')])
     return [str(getpid()).encode()]
