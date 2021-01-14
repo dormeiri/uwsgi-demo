b52e98f856c3b46df60d5ec24edc734eff20b457
dmeiri <dmeiri@cisco.com>
2021-01-14 17:59:26 +0200

    readability fix

--- flask/flask_app.py
+++ flask/flask_app.py
@@ -2,14 +2,17 @@ from flask import Flask
 from time import sleep
 from os import getpid
 
-DELAY = 2
 
-sleep(5)  # demonstrate slow app time with graceful reload
+UP_TIME = 5
+RESPONSE_TIME = 2
 
+
+sleep(UP_TIME)  # demonstrates slow up time
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    sleep(DELAY)
+    sleep(RESPONSE_TIME)  # demonstrates slow response time
     return str(getpid())
+
--- pure/wsgi_app.py
+++ pure/wsgi_app.py
@@ -1,8 +1,4 @@
-from time import sleep
-from os import getpid
-
-
 def application(env, start_response):
     start_response('200 OK', [('Content-Type', 'text/html')])
-    return [b'Hello World!']
+    return [b'Hello, World!']
 
